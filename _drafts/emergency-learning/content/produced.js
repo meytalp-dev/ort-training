772f1f30e3b9783e0dc43d7a735855905172c1bd
Meytal <meytalp.dev@gmail.com>
2026-07-16 11:27:57 +0300

    רֶצֶף — עדכון produced.js: שיער מ7=11 (מודולה שלמה), מ5=11

--- _drafts/emergency-learning/content/produced.js
+++ _drafts/emergency-learning/content/produced.js
@@ -3,8 +3,8 @@
 window.PRODUCED = {
   "hair-design|1": 15,
   "hair-design|4": 9,
-  "hair-design|5": 9,
-  "hair-design|7": 7,
+  "hair-design|5": 11,
+  "hair-design|7": 11,
   "hebrew|5": 1,
   "media-design|1": 3,
   "welding|1": 3
